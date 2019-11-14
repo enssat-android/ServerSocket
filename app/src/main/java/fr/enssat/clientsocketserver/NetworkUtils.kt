670d0e140afb4d3bb243ce10104924820d3f4566
gilles le brun <gil.lebrun@orange.com>
2019-11-14 09:57:23 +0100

    support for device <23 sdk api

--- app/src/main/java/fr/enssat/clientsocketserver/NetworkUtils.kt
+++ app/src/main/java/fr/enssat/clientsocketserver/NetworkUtils.kt
@@ -4,15 +4,32 @@ import android.content.Context
 import android.net.ConnectivityManager
 import java.net.Inet4Address
 import java.net.InetAddress
+import android.content.Context.WIFI_SERVICE
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.text.format.Formatter
 
 
 class NetworkUtils {
     companion object{
         fun getIpAddress(context: Context): InetAddress? {
-            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val linkInfos = cm.getLinkProperties(cm.activeNetwork)?.linkAddresses
-            val address =  linkInfos?.filter {it -> it.address is Inet4Address }?.single()
-            return address?.address
+            var result : InetAddress?
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val cm =
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                val linkInfos = cm.getLinkProperties(cm.activeNetwork)?.linkAddresses
+                val address = linkInfos?.filter { it -> it.address is Inet4Address }?.single()
+                result = address?.address
+            }
+            else {
+
+                val wm = context.getSystemService(WIFI_SERVICE) as WifiManager
+                val ip = Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
+                result = InetAddress.getByName(ip)
+            }
+
+            return result
         }
     }
 }
